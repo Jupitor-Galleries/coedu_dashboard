ff9c32c995c582ccbf4561667574134120ec6697
JUSTINE IMASIKU <justineimasiku21@gmail.com>
2024-07-29 07:48:03 +0000

    Update Classes.jsx

--- src/pages/classes/Classes.jsx
+++ src/pages/classes/Classes.jsx
@@ -3,11 +3,25 @@ import "./Classes.css";
 import { NavLink } from 'react-router-dom';
 import ClassModal from './modal/ClassModal';
 import { getUserClasses } from '../../api/class'
+import SideNav from '../dashboard_page/components/sidenav/SideNav';
 
 const Classes = () => {
 
     const [modalOpened, setModalOpened] = useState(false);
-    const [classes, setClasses] = useState([])
+    const [classes, setClasses] = useState([
+        {
+            name: "Tech Nomads",
+            date: "22-05-2024",
+            sdate: "22-05-2024",
+            time: "12:35"
+        },
+        {
+            name: "Asikana Network",
+            date: "22-05-2024",
+            sdate: "22-05-2024",
+            time: "12:35"
+        },
+    ])
     
     const fetchUserClasses = async () => {
             const res = await getUserClasses()
@@ -23,23 +37,49 @@ const Classes = () => {
 
 
   return (
-    <div className='classes-container'>
-         <div className="classes-data">
-            <button className='create-btn' onClick={() => setModalOpened(true)}>Create New Class</button>
-            <div className="classes">
+    <div className='dashboard-container'>
+        <SideNav organization={"organization"} />
+        <div className="mainpage-container">
+            <div className="students-container">
+                <div className="h">
+                    <h4>School Name</h4>
+                    <button className='create-btn' onClick={() => setModalOpened(true)}>Create New Class</button>
+                </div>
+            <table className="students">
+                <tr>
+                    <th>Name</th>
+                    <th>Date Created</th>
+                    <th>Start Date</th>
+                    <th>Time</th>
+                    <th>Action</th>
+                </tr>
                 {
-                    classes.map((clas) => {
+                    classes.map((student) => {
                         return (
-                            <div className="class">
-                                <p>{clas.name}</p>
-                                <NavLink to={`/class/${clas._id}`}>Manage</NavLink>
-                            </div>
+                            <tr>
+                                <td>
+                                    {student.name}
+                                </td>
+                                <td>
+                                    {student.date}
+                                </td>
+                                <td>
+                                    {student.sdate}
+                                </td>
+                                <td>
+                                    {student.time}
+                                </td>
+                                <td>
+                                    <button className='create-btn'>Manage Class</button>
+                                </td>
+                            </tr>
                         )
                     })
                 }
+            </table>
             </div>
-         </div>
-         <ClassModal modalOpened={modalOpened} onClose={() => setModalOpened(false)} />
+            <ClassModal modalOpened={modalOpened} onClose={() => setModalOpened(false)} />
+        </div>
     </div>
   )
 }
