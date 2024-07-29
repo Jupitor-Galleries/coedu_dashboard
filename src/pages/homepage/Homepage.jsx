5eb2e7a0e254ee075f28a5ce972791fff167bfc8
JUSTINE IMASIKU <justineimasiku21@gmail.com>
2024-07-29 17:19:08 +0000

    add features section

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './Homepage.css';
 import Hero from './components/hero/Hero';
 import About from './components/about/About';
+import Features from './components/features/Features';
 
 const Homepage = () => {
   return (
     <div className='homepage-container'>
         <Hero />
         <About />
+        <Features />
     </div>
   )
 }
