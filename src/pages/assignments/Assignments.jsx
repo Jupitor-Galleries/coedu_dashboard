57c9f70b8b7b7bc6b38b79768ae49a14cacba7b4
Courtney Letsa <courtneyletsa@gmail.com>
2024-08-05 18:40:16 +0000

    update

--- src/pages/assignments/Assignments.jsx
+++ src/pages/assignments/Assignments.jsx
@@ -1,61 +1,63 @@
-import React, { useState, useEffect } from 'react';
-import { NavLink } from 'react-router-dom';
-import './Assignments.css'
-import StudentModal from '../dashboard_page/components/modal/StudentModal';
-import { useParams } from 'react-router-dom';
-import { getAssignmentsByClass } from '../../api/class'
-import SideNav from '../dashboard_page/components/sidenav/SideNav';
-import AssignmentsModal from '../dashboard_page/components/modal/AssignmentsModal';
-import { FaBell } from 'react-icons/fa';
-import RightNav from '../dashboard_page/components/rightnav/RightNav';
-import { getCurrentUser } from '../../api/auth';
+import React, { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
+import "./Assignments.css";
+import StudentModal from "../dashboard_page/components/modal/StudentModal";
+import { useParams } from "react-router-dom";
+import { getAssignmentsByClass } from "../../api/class";
+import SideNav from "../dashboard_page/components/sidenav/SideNav";
+import AssignmentsModal from "../dashboard_page/components/modal/AssignmentsModal";
+import { FaBell } from "react-icons/fa";
+import RightNav from "../dashboard_page/components/rightnav/RightNav";
+import { getCurrentUser } from "../../api/auth";
 
 const Assignments = () => {
-    const classId = useParams().classId;
-    const [modalOpened, setModalOpened] = useState(false);
-    const [assignements, setAssignments] = useState([])
+  const classId = useParams().classId;
+  const [modalOpened, setModalOpened] = useState(false);
+  const [assignements, setAssignments] = useState([]);
 
-    const broadcast = () => {
-
+  const broadcast = () => {};
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    return `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
+  };
+  const allAssignments = async () => {
+    const res = await getAssignmentsByClass(classId);
+    console.log(res);
+    if (res?.status) {
+      setAssignments(res.data);
     }
-    const formatDate = (dateString) => {
-        const date = new Date(dateString);
-        return `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
-      };
-    const allAssignments = async() => {
-        const res = await getAssignmentsByClass(classId)
-        console.log(res)
-        if(res?.status) {
-            setAssignments(res.data)
-        }
-    }
-    useEffect(() => {
-        allAssignments()
-    },[])
+  };
+  useEffect(() => {
+    allAssignments();
+  }, []);
 
-    const [currentUser, setCurrentUser] = useState(null);
+  const [currentUser, setCurrentUser] = useState(null);
 
-    const getCurrentUs = async() => {
-      const res = await getCurrentUser();
-      if(res.status) {
-        console.log(res.data);
-        setCurrentUser(res.data)
-      }
+  const getCurrentUs = async () => {
+    const res = await getCurrentUser();
+    if (res.status) {
+      console.log(res.data);
+      setCurrentUser(res.data);
     }
-  
-    useEffect(() => {
-      getCurrentUs()
-    }, [])
+  };
+
+  useEffect(() => {
+    getCurrentUs();
+  }, []);
   return (
-    <div className='dashboard-container'>
-        <SideNav organization={"organization"} classId={classId} currentUser={currentUser} />
-        <div className="dashboard-page-data2">
-            <div className="students-container">
-            <div className="notif-cont">
+    <div className="dashboard-container">
+      <SideNav
+        organization={"organization"}
+        classId={classId}
+        currentUser={currentUser}
+      />
+      <div className="dashboard-page-data2">
+        <div className="students-container">
+          {/* <div className="notif-cont">
             <div className="ico">
               <FaBell />
             </div>
-          </div>
+          </div> */}
           <div className="h">
             <h3>{currentUser?.organization.name}</h3>
             <div className="flex-row">
@@ -73,40 +75,42 @@ const Assignments = () => {
               </button>
             </div>
           </div>
-            <table className="students">
+          <table className="students">
+            <tr>
+              <th>Title</th>
+              <th>Date Created</th>
+              <th>Due Date</th>
+              <th>Action</th>
+            </tr>
+            {assignements.map((assignment) => {
+              return (
                 <tr>
-                    <th>Title</th>
-                    <th>Date Created</th>
-                    <th>Due Date</th>
-                    <th>Action</th>
+                  <td>{assignment.title}</td>
+                  <td>{formatDate(assignment.createdAt)}</td>
+                  <td>{formatDate(assignment.dueDate)}</td>
+                  <td>
+                    <NavLink
+                      to={`/assignment/${classId}/${assignment._id}`}
+                      className="create-btn3"
+                    >
+                      View Assignment
+                    </NavLink>
+                  </td>
                 </tr>
-                {
-                    assignements.map((assignment) => {
-                        return (
-                            <tr>
-                                <td>
-                                    {assignment.title}
-                                </td>
-                                <td>
-                                    {formatDate(assignment.createdAt)}
-                                </td>
-                                <td>
-                                    {formatDate(assignment.dueDate)}
-                                </td>
-                                <td>
-                                    <NavLink to={`/assignment/${classId}/${assignment._id}`} className='create-btn3'>View Assignment</NavLink>
-                                </td>
-                            </tr>
-                        )
-                    })
-                }
-            </table>
-            </div>
-            <RightNav />
-            <AssignmentsModal modalOpened={modalOpened} onClose={() => setModalOpened(false)} allAssignments={allAssignments} classId={classId}/>
+              );
+            })}
+          </table>
         </div>
+        <RightNav />
+        <AssignmentsModal
+          modalOpened={modalOpened}
+          onClose={() => setModalOpened(false)}
+          allAssignments={allAssignments}
+          classId={classId}
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Assignments
+export default Assignments;
